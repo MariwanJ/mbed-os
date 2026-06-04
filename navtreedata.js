c3635ac78970037b9e4fbb5ed85081d0186491b0
MariwanJ <MariwanJ@users.noreply.github.com>
2026-06-04 18:47:35 +0000

    deploy: 256d108e96ea735370e7da92a4c7fb2f87384b4b

---
+++ navtreedata.js
@@ -0,0 +1,133 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Mbed OS Reference", "index.html", [
+    [ "Mbed OS code base", "index.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", "functions_eval" ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_a_l_t1250___p_p_p_8h_source.html",
+"_s_a_r_a4___p_p_p___cellular_network_8h_source.html",
+"_useful_buf_8h.html#a8ac63be86d630aeeb748cfd4491360f0",
+"class_cellular_interface.html#a526802de6e27e291d46e3e3e6a396978",
+"class_e_m_a_c_interface.html#a26000e958701f5b35886fac1757c0bf4",
+"class_e_s_p32_stack.html#a345f6e49fcae39c08ca892e2d1b6337b",
+"class_i2_c_e_e_block_device.html#ae3e772fe7bcf262931cb3620b2af59f7",
+"class_lo_ra_w_a_n_interface.html#af10c1a0a7b0539bb6772ed62993fe150",
+"class_nanostack_1_1_p_p_p_interface.html#af65ec3ef6f591ccca424610f1581981e",
+"class_operation_list.html",
+"class_s_d_block_device.html#a61522974c93e870b2a99db0e690c0cac",
+"class_t_l_s_socket.html#aa276d81e7e532312a696754b5ca39313ae713ef19fd1d1198f7605f2bf4e5e2a5",
+"class_u_s_b_c_d_c___e_c_m.html#a978ae250c8533b5bd84fd29c60bae220",
+"class_u_s_b_m_s_d.html#ae6f018536d482152b993f522d8d2942d",
+"class_u_s_b_serial.html#aadb624fc379dbf725a2cb132cc37929f",
+"class_write_only_gatt_characteristic.html#a1105e137fd626076bda6bf1c4eee2aab",
+"classble_1_1_gap.html#a35765c8a64f53448fbb56daffdf0c85e",
+"classble_1_1_security_manager.html#a686cd5fd54600e2376ac36ed86688d8ca388dcaa70f8eeb58aa6c703047689741",
+"classevents_1_1_task_base.html#abcca23e6afadfade8c5d63ee62b49bfd",
+"classmbed_1_1_a_t___cellular_device.html#ab58075b3272921b7b84a8c800fb98bf1",
+"classmbed_1_1_bus_in_out.html",
+"classmbed_1_1_chaining_block_device.html#a044d6f3d90a260727a7de63c42c6c345",
+"classmbed_1_1_flash_i_a_p.html#aff9daead4da19d7c8cdc148c4ed11a03",
+"classmbed_1_1_i2_c.html#aee84581f841e97427a9f6191665e8006a41246e9c8691b7e33bc79b345e06b48e",
+"classmbed_1_1_o_n_b_o_a_r_d___t_e_l_i_t___m_e310.html#a4c09dbff28142164fae3e477b74dd7b1",
+"classmbed_1_1_r_m1000___a_t___cellular_context.html#a8dc0651d23c76c830d241236e22f0a1b",
+"classmbed_1_1_serial_base.html#a2b8509410439be6477d7532b0ccec15c",
+"classmbed_1_1_t_e_l_i_t___m_e310___cellular_context.html#abb962f363d7a7ec29b27844a1e5f7ecc",
+"classmbed_1_1_unbuffered_serial.html#a583f0d0f54decb0c9034677f85db5c9b",
+"classmbed_1_1nfc_1_1ndef_1_1common_1_1_u_r_i.html#af98f8afa293c3273af5b45d10fd4832ea7c3c966dfd496ce366a443c73779f02b",
+"compatibility_2ble_2_service_discovery_8h_source.html",
+"dir_96502225faf1c0723cd87cb00d4ea33a.html",
+"functions_vars_q.html",
+"group___net_socket.html#gga61fab8ca5ae5d56fb51a86dcd36f56eaa370a4b73b10e0e04f9b2406f5d5c8560",
+"group__crypto__types.html#ga702ff75385a6ae7d4247033f479439af",
+"group__gatt.html",
+"group__hal__qspi.html#gad779f015d5cf002bc91c015b7f4c3f57",
+"group__mbedtls__asn1__module.html#gac4e932ef9cdf9fa9b8fb152357c6ec64",
+"group__mbedtls__ctr__drbg__module.html#ga2eff98a04c90d33f059690edda888fec",
+"group__mbedtls__havege__module.html#ga061c764314cfb0c50ad6815596a99b50",
+"group__mbedtls__oid__module.html#gacdfbc5ead965aaec5b1f82a575a8b2d2",
+"group__mbedtls__ssl__module.html#ga2dc104a181bcd11eafbbf7e6923978bc",
+"group__mbedtls__ssl__module.html#gafee3c17ec14a8debd09140dced64fc76",
+"group__platform__atomic.html#ga2a81771e47a93b508d6ec3f4666b506e",
+"group__platform__mem__trace.html#gad89cb3334584a96e61d51eaf86da99c0",
+"lifecycle_8h.html#a394f7e1bcb43be239552e9cc4ff80c18",
+"namespacemembers_func.html",
+"qcbor_8h.html#a917c840b71af72c2cdd4b31bb2a4fb42a0d74cc5b94f9510ecc5f330c238508b8",
+"struct_gatt_updates_changed_callback_params.html",
+"structble_1_1_safe_enum.html#aa20add9035805740e3f5e814b9bb35d9",
+"structble_1_1peer__address__type__t.html",
+"structloramac__mcps__confirm__t.html#a4449d9ebc739efb760ac6694b32c4305",
+"structmbed_1_1nfc_1_1_n_f_c_e_e_p_r_o_m_1_1_delegate.html",
+"structmbedtls__cipher__info__t.html#ac905bc99974772ab82e8c0863261a521",
+"structmbedtls__ssl__context.html#a18dc2da7561be0d14976825dfaec7857",
+"structpacket__params.html#a8b7cf7db4a49e6015d011f3b204d276d",
+"structticker__event__queue__t.html#a61675e2918833e9447253367555ddac6",
+"tfm__platform__api_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
